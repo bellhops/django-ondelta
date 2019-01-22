652a1f2f72b92ecfbb0394aa221fe3b9de88aa74
John Hyatt <Woodzyowl0309@yahoo.com>
2019-01-22 19:14:14 +0000

    Update models.py (#1)

--- ondelta/models.py
+++ ondelta/models.py
@@ -36,7 +36,7 @@ class OnDeltaMixin(models.Model):
         return [f.name for f in self._meta.fields if f.name not in {'c', 'id'}]
 
     def _ondelta_take_snapshot(self):
-        self._ondelta_shadow = copy.copy(self)
+        self._ondelta_shadow = copy.deepcopy(self)
 
     def _ondelta_get_differences(self):
 
